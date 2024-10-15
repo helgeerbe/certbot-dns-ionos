0e93705e195bb6f23e79e7968ab65b7bae9d104d
DorianCoding <108593662+DorianCoding@users.noreply.github.com>
2024-10-15 13:17:52 +0200

    Typo error

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.rst")) as f:
+with open(path.join(this_directory, "README.md")) as f:
     long_description = f.read()
 
 setup(
